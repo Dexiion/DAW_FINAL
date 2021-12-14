b6a6ed07d9d981a9149fe9aa59d6f5cf1553a2b0
Otoniel Pérez <otonielcuentas@gmail.com>
2021-12-14 21:30:34 +0000

    WIP

---
+++ app/helpers/filtered_cars_helper.rb
@@ -0,0 +1,2 @@
+module FilteredCarsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'filtered_cars/index'
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "cars/new", to:"cars#new"
   get "cars/:id", to:"cars#show"
   get "cars", to:"cars#index"
   post "cars", to:"cars#create"
+  get "posts", to:"posts#index"
+
+  get "/login", to: 'sessions#login'
+  post "/login", to: 'sessions#create'
+  post "/logout", to: 'sessions#destroy'
+  get "/logout", to: 'sessions#destroy'
+
   root 'home#index'
 
 end
