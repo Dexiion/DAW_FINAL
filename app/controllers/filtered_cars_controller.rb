61c672bfa48a2f83cb54c2298bc9653aac1434ba
Otoniel Pérez <otonielcuentas@gmail.com>
2021-12-16 19:50:58 +0000

    Ignore

--- app/controllers/filtered_cars_controller.rb
+++ app/controllers/filtered_cars_controller.rb
@@ -1,12 +1,12 @@
 class FilteredCarsController < ApplicationController
   def index
     @cars = Car.all.page(params[:page])
-    @filteredCars = @cars.where(parseCondition("localizacion",params[:location])).where(parseCondition("marca",params[:brand])).where(parseCondition("combustible",params[:fuel])).where(parseMinPrice("precio",params[:minprice])).where(parseMaxPrice("precio",params[:maxprice])).where(parseCondition("puertas",params[:doors]))
+    @filteredCars = @cars.where(parseCondition("localizacion",params[:location])).where(parseCondition("marca",params[:brand])).where(parseCondition("combustible",params[:fuel])).where(parseMinPrice("precio",params[:minprice])).where(parseMaxPrice("precio",params[:maxprice])).where(parseDoors("puertas",params[:doors]))
 
   end
 
   def parseCondition(paramName,paramValue)
-    if (paramValue.eql?("Provincia") || paramValue.eql?("Marca") || paramValue.eql?("Combustible") || paramValue.eql?("Puertas") || paramValue.eql?(nil))
+    if (paramValue.eql?("Provincia") || paramValue.eql?("Marca") || paramValue.eql?("Combustible") || paramValue.eql?(nil))
       return paramName + " <> " + "'dmwaoidnawkodnawonfoiwan'"
     else
       return paramName + " = " +  "'" + paramValue + "'"
@@ -26,6 +26,14 @@ class FilteredCarsController < ApplicationController
       return paramName + " <> " + "-5"
     else
       return paramName + " < " + "'" + paramValue + "'"
+    end
+  end
+
+  def parseDoors(paramName, paramValue)
+    if (paramValue.eql?("Puertas") || paramValue.eql?(nil))
+      return paramName + " != " + "-5"
+    else
+      return paramName + " = " + "'" + paramValue + "'"
     end
   end
 end
